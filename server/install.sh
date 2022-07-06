d6d03b7434b57c39b9f30c91227c4674ac1c0477
Sujal Singh <email.sujalsingh@gmail.com>
2022-07-06 10:02:22 +0530

    fix urls

--- server/install.sh
+++ server/install.sh
@@ -1,9 +1,9 @@
 mkdir ~/.competishun-server
 cd ~/.competishun-server || exit
-wget https://github.com/sujaldev/competishun-pro/
-wget https://github.com/sujaldev/competishun-pro/
+wget https://github.com/sujaldev/competishun-pro/blob/server/server/script_modifier_server.py
+wget https://github.com/sujaldev/competishun-pro/blob/server/server/requirements.txt
 virtualenv env
 source env/bin/activate
 pip3 install -r requirements.txt
 
-wget https://github.com/sujaldev/competishun-pro/
+wget https://github.com/sujaldev/competishun-pro/blob/server/server/competishun-server.service
