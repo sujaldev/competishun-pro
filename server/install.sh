e73140eca433028c870b253135004cc82f726150
Sujal Singh <email.sujalsingh@gmail.com>
2022-07-06 10:30:35 +0530

    fix installation

--- server/install.sh
+++ server/install.sh
@@ -1,5 +1,5 @@
-mkdir ~/.competishun-server
-cd ~/.competishun-server || exit
+mkdir /opt/competishun-server
+cd /opt/competishun-server || exit
 wget https://github.com/sujaldev/competishun-pro/blob/server/server/script_modifier_server.py
 wget https://github.com/sujaldev/competishun-pro/blob/server/server/requirements.txt
 virtualenv env
@@ -7,3 +7,6 @@ source env/bin/activate
 pip3 install -r requirements.txt
 
 wget https://github.com/sujaldev/competishun-pro/blob/server/server/competishun-server.service
+mv competishun-server.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl status competishun-server
